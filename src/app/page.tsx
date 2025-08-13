6e029f2647c60a05b3c39af94035713e7fadc0f8
Justikarx <twin.m999@gmail.com>
2025-08-13 14:14:24 +0700

    Промежуточный коммит: сделаны адаптивы секции отзывов

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,8 +19,8 @@ export default function Home() {
         <Compilation />
         <Work />
         <Partners />
-        {/* 
         <Rating />
+        {/* 
         <Download />
         <Selection /> */}
       </div>
