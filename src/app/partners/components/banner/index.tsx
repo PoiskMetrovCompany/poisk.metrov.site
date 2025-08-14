9dcff06edfc2d4e580f28eb7987fed57eec3a447
lzfilms4 <lzfilms4@gmail.com>
2025-08-14 15:37:21 +0700

    unfinished: adaptive partners page

--- src/app/partners/components/banner/index.tsx
+++ src/app/partners/components/banner/index.tsx
@@ -1,17 +1,30 @@
 import React from "react"
 import styles from "./banner.module.scss"
 import Heading1 from "@/components/ui/heading1"
-import Heading3 from "@/components/ui/heading3"
+import clsx from "clsx"
 
 const Banner = () => {
   return (
-    <div className={styles.banner}>
-      <Heading1 className={styles.banner__title}>
-        <b>Поиск Метров</b> –
-        <br />
-        найдем квартиру, поможем с ипотекой
-      </Heading1>
-      <p className={styles.banner__subtitle}>
+    <div className={styles.banner__container}>
+      <div className={styles.banner}>
+        <Heading1 className={styles.banner__title}>
+          <b>Поиск Метров</b> –
+          <br />
+          найдем квартиру, поможем с ипотекой
+        </Heading1>
+        <p className={styles.banner__subtitle}>
+          Вы можете передать клиента в наше сопровождение, если он планирует
+          переезд в другой город. Мы предусматриваем агентское вознаграждение
+          за каждого переданного клиента!
+        </p>
+      </div>
+
+      <p
+        className={clsx(
+          styles.banner__subtitle,
+          styles.banner__subtitle_mobile
+        )}
+      >
         Вы можете передать клиента в наше сопровождение, если он планирует
         переезд в другой город. Мы предусматриваем агентское вознаграждение
         за каждого переданного клиента!
