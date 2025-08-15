789978247ad7897a005117d0f47d78480f3262c9
Justikarx <twin.m999@gmail.com>
2025-08-15 13:49:22 +0700

    Промежуточный коммит: начата верстка страницы продажи квартиры

--- src/components/sendSellApplication/index.tsx
+++ src/components/sendSellApplication/index.tsx
@@ -1,24 +1,36 @@
 "use client"
-import React, {FC} from "react";
+import React, { FC } from "react"
 import styles from "./sendApplication.module.scss"
-import FormSendSellApp from "./rightSide";
+import ActionButton from "../ui/buttons/ActionButton"
+import FormSendSellApp from "./rightSide"
 
-
-const SendSellApplication:FC = () => {
-  return(
-    <div className={styles.sendSellApp}>
-        <div className={styles.sendSellApp__container}>
-            <div className={styles.sendSellApp__leftSide}>
-              <div className={styles.sendSellApp__header}>
-                Оставьте заявку
-              </div>
-            </div>
-            <div className={styles.sendSellApp__rightSide}>
-                <FormSendSellApp
-                  
-                />
-            </div>
+const SendSellApplication: FC = () => {
+  return (
+    <div>
+      <div className={styles.sendSellApp__container__image}>
+        <div className={styles.sendSellApp__container__image__text}>
+          Хотите продать свою недвижимость?
         </div>
+        <div className={styles.sendSellApp__container__image__button}>
+          <ActionButton
+            type="primary"
+            size="medium"
+            className={styles.sendSellApp__container__image__button__send}
+          >
+            Оставить заявку
+          </ActionButton>
+        </div>
+      </div>
+      <div className={styles.sendSellApp}>
+        <div className={styles.sendSellApp__container}>
+          <div className={styles.sendSellApp__leftSide}>
+            <div className={styles.sendSellApp__header}>Оставьте заявку</div>
+          </div>
+          <div className={styles.sendSellApp__rightSide}>
+            <FormSendSellApp />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
