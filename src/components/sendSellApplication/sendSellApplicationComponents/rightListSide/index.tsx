e2da322add2b5d628e663f04a1ebb42c6cc37f4f
Justikarx <twin.m999@gmail.com>
2025-08-22 09:31:20 +0700

    Финальный коммит: сверстана страница "Продать"

--- src/components/sendSellApplication/sendSellApplicationComponents/rightListSide/index.tsx
+++ src/components/sendSellApplication/sendSellApplicationComponents/rightListSide/index.tsx
@@ -1,27 +1,249 @@
-import React, {FC} from "react";
+import React, { FC } from "react"
 import styles from "./rightListSide.module.scss"
 
-
 const RightListSide = () => {
-  return(
-
+  return (
     <div className={styles.sendSellApp__container__rightSide}>
-       <div className={styles.sendSellApp__container__rightSide__list}>
+      <div className={styles.sendSellApp__container__rightSide__list}>
         <div className={styles.sendSellApp__container__rightSide__list__item}>
-          <div className={styles.sendSellApp__container__rightSide__list__item__number}>
+          <div
+            className={
+              styles.sendSellApp__container__rightSide__list__item__number
+            }
+          >
             01
           </div>
-          <div className={styles.sendSellApp__container__rightSide__list__item__text}>
-            <div className={styles.sendSellApp__container__rightSide__list__item__text__header}>
+          <div
+            className={
+              styles.sendSellApp__container__rightSide__list__item__text
+            }
+          >
+            <div
+              className={
+                styles.sendSellApp__container__rightSide__list__item__text__header
+              }
+            >
               Оформление заявки
             </div>
-            <div className={styles.sendSellApp__container__rightSide__list__item__text__title}>
-              Оставьте заявку на сайте для первичной консультации с нашим специалистом.
+            <div
+              className={
+                styles.sendSellApp__container__rightSide__list__item__text__title
+              }
+            >
+              Оставьте заявку на сайте для первичной консультации с нашим
+              специалистом.
             </div>
           </div>
         </div>
-       </div>
-    </div>  
+        <div className={styles.sendSellApp__container__rightSide__list__item}>
+          <div
+            className={
+              styles.sendSellApp__container__rightSide__list__item__number
+            }
+          >
+            02
+          </div>
+          <div
+            className={
+              styles.sendSellApp__container__rightSide__list__item__text
+            }
+          >
+            <div
+              className={
+                styles.sendSellApp__container__rightSide__list__item__text__header
+              }
+            >
+              Оценка недвижимости
+            </div>
+            <div
+              className={
+                styles.sendSellApp__container__rightSide__list__item__text__title
+              }
+            >
+              Проведем предварительную оценку рыночной стоимости вашей
+              недвижимости, согласуем цену и условия сделки.
+            </div>
+          </div>
+        </div>
+        <div className={styles.sendSellApp__container__rightSide__list__item}>
+          <div
+            className={
+              styles.sendSellApp__container__rightSide__list__item__number
+            }
+          >
+            03
+          </div>
+          <div
+            className={
+              styles.sendSellApp__container__rightSide__list__item__text
+            }
+          >
+            <div
+              className={
+                styles.sendSellApp__container__rightSide__list__item__text__header
+              }
+            >
+              Подготовка документов
+            </div>
+            <div
+              className={
+                styles.sendSellApp__container__rightSide__list__item__text__title
+              }
+            >
+              Мы подготовим все документы, необходимые для дальнейшей продажи
+              имущества.
+            </div>
+          </div>
+        </div>
+        <div className={styles.sendSellApp__container__rightSide__list__item}>
+          <div
+            className={
+              styles.sendSellApp__container__rightSide__list__item__number
+            }
+          >
+            04
+          </div>
+          <div
+            className={
+              styles.sendSellApp__container__rightSide__list__item__text
+            }
+          >
+            <div
+              className={
+                styles.sendSellApp__container__rightSide__list__item__text__header
+              }
+            >
+              Фотосъемка недвижимости
+            </div>
+            <div
+              className={
+                styles.sendSellApp__container__rightSide__list__item__text__title
+              }
+            >
+              Проведем профессиональную фотосъемку недвижимости, покажем все
+              преимущества вашего объекта.
+            </div>
+          </div>
+        </div>
+        <div className={styles.sendSellApp__container__rightSide__list__item}>
+          <div
+            className={
+              styles.sendSellApp__container__rightSide__list__item__number
+            }
+          >
+            05
+          </div>
+          <div
+            className={
+              styles.sendSellApp__container__rightSide__list__item__text
+            }
+          >
+            <div
+              className={
+                styles.sendSellApp__container__rightSide__list__item__text__header
+              }
+            >
+              Рекламная компания
+            </div>
+            <div
+              className={
+                styles.sendSellApp__container__rightSide__list__item__text__title
+              }
+            >
+              Создадим качественную рекламную компанию, которая сократит время
+              поиска покупателя.
+            </div>
+          </div>
+        </div>
+        <div className={styles.sendSellApp__container__rightSide__list__item}>
+          <div
+            className={
+              styles.sendSellApp__container__rightSide__list__item__number
+            }
+          >
+            06
+          </div>
+          <div
+            className={
+              styles.sendSellApp__container__rightSide__list__item__text
+            }
+          >
+            <div
+              className={
+                styles.sendSellApp__container__rightSide__list__item__text__header
+              }
+            >
+              Самостоятельно проведем показ
+            </div>
+            <div
+              className={
+                styles.sendSellApp__container__rightSide__list__item__text__title
+              }
+            >
+              Назначим удобное время, защитим интересы продавца
+            </div>
+          </div>
+        </div>
+        <div className={styles.sendSellApp__container__rightSide__list__item}>
+          <div
+            className={
+              styles.sendSellApp__container__rightSide__list__item__number
+            }
+          >
+            07
+          </div>
+          <div
+            className={
+              styles.sendSellApp__container__rightSide__list__item__text
+            }
+          >
+            <div
+              className={
+                styles.sendSellApp__container__rightSide__list__item__text__header
+              }
+            >
+              Оформим сделку
+            </div>
+            <div
+              className={
+                styles.sendSellApp__container__rightSide__list__item__text__title
+              }
+            >
+              Обеспечим юридическое сопровождение сделки.
+            </div>
+          </div>
+        </div>
+        <div className={styles.sendSellApp__container__rightSide__list__item}>
+          <div
+            className={
+              styles.sendSellApp__container__rightSide__list__item__number
+            }
+          >
+            08
+          </div>
+          <div
+            className={
+              styles.sendSellApp__container__rightSide__list__item__text
+            }
+          >
+            <div
+              className={
+                styles.sendSellApp__container__rightSide__list__item__text__header
+              }
+            >
+              Оплата услуги
+            </div>
+            <div
+              className={
+                styles.sendSellApp__container__rightSide__list__item__text__title
+              }
+            >
+              У нас фиксированная стоимость услуги без дополнительных комиссий
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
