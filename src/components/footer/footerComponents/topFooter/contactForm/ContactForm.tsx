207b4e9fe05de570a68849a1a3ed1e42d21dcd44
Justikarx <twin.m999@gmail.com>
2025-08-25 11:37:04 +0700

    Финальный коммит: сверстана секция ЛК. Доработать адаптивы

--- src/components/footer/footerComponents/topFooter/contactForm/ContactForm.tsx
+++ src/components/footer/footerComponents/topFooter/contactForm/ContactForm.tsx
@@ -41,82 +41,86 @@ const ContactForm: FC = () => {
   }
 
   return (
-    <form className={styles.contactForm}>
-      <FormRow className={clsx(styles.smallWrap)}>
-        <InputContainer
-          label="Ваша фамилия"
-          placeholder="Введите вашу фамилию"
-          value={formData.lastName}
-          onChange={(value) => handleInputChange("lastName", value)}
-          name="lastName"
-          className={styles.w_50}
-        />
-        <InputContainer
-          label="Ваше имя"
-          placeholder="Введите ваше имя"
-          value={formData.firstName}
-          onChange={(value) => handleInputChange("firstName", value)}
-          name="firstName"
-          className={styles.w_50}
-        />
-      </FormRow>
+    <div className={styles.container}>
+      <form className={styles.container__contactForm}>
+        <FormRow className={clsx(styles.smallWrap)}>
+          <InputContainer
+            label="Ваша фамилия"
+            placeholder="Введите вашу фамилию"
+            value={formData.lastName}
+            onChange={(value) => handleInputChange("lastName", value)}
+            name="lastName"
+            className={styles.w_50}
+          />
+          <InputContainer
+            label="Ваше имя"
+            placeholder="Введите ваше имя"
+            value={formData.firstName}
+            onChange={(value) => handleInputChange("firstName", value)}
+            name="firstName"
+            className={styles.w_50}
+          />
+        </FormRow>
 
-      <FormRow className={clsx(styles.smallWrap)}>
-        <InputContainer
-          label="Ваше отчество"
-          placeholder="Введите ваше отчество"
-          value={formData.middleName}
-          onChange={(value) => handleInputChange("middleName", value)}
-          name="middleName"
-          className={styles.w_50}
-        />
-        <InputContainer
-          label="Телефон"
-          placeholder="+7"
-          value={formData.phone}
-          onChange={(value) => handleInputChange("phone", value)}
-          name="phone"
-          type="phone"
-          className={styles.w_50}
-        />
-        <ArrowButton
-          onClick={handleSubmit}
-          size="medium"
-          absolute={true}
-          className={clsx(
-            !formData.isAgreed ? styles.contactForm__submitButton_disabled : "",
-            styles.position
-          )}
-        />
-      </FormRow>
+        <FormRow className={clsx(styles.smallWrap)}>
+          <InputContainer
+            label="Ваше отчество"
+            placeholder="Введите ваше отчество"
+            value={formData.middleName}
+            onChange={(value) => handleInputChange("middleName", value)}
+            name="middleName"
+            className={styles.w_50}
+          />
+          <InputContainer
+            label="Телефон"
+            placeholder="+7"
+            value={formData.phone}
+            onChange={(value) => handleInputChange("phone", value)}
+            name="phone"
+            type="phone"
+            className={styles.w_50}
+          />
+          <ArrowButton
+            onClick={handleSubmit}
+            size="medium"
+            absolute={true}
+            className={clsx(
+              !formData.isAgreed
+                ? styles.contactForm__submitButton_disabled
+                : "",
+              styles.position
+            )}
+          />
+        </FormRow>
 
-      <FormRow>
-        <ActionButton
-          onClick={handleSubmit}
-          size="large"
-          type="primary"
-          svgWidth={20}
-          svgHeight={20}
-          className={clsx(styles.borderRadius, styles.hideOnDesktop)}
-          svgSrc="./svgFiles/nextArrow.svg"
-          svgDiscolored={true}
-        >
-          Отправить
-        </ActionButton>
-      </FormRow>
+        <FormRow>
+          <ActionButton
+            onClick={handleSubmit}
+            size="large"
+            type="primary"
+            svgWidth={20}
+            svgHeight={20}
+            className={clsx(styles.borderRadius, styles.hideOnDesktop)}
+            svgSrc="./svgFiles/nextArrow.svg"
+            svgDiscolored={true}
+          >
+            Отправить
+          </ActionButton>
+        </FormRow>
 
-      <FormRow className={styles.mt_0} justifyContent="flex-start">
-        <CheckboxRow
-          checked={formData.isAgreed}
-          onChange={handleCheckboxChange}
-          text="Нажимая на кнопку, вы даете согласие на обработку"
-          linkText="своих персональных данных"
-          linkHref="/privacy-policy"
-          name="personalData"
-          id="personalData"
-        />
-      </FormRow>
-    </form>
+        <FormRow className={styles.mt_0} justifyContent="flex-start">
+          <CheckboxRow
+            checked={formData.isAgreed}
+            onChange={handleCheckboxChange}
+            text="Нажимая на кнопку, вы даете согласие на обработку"
+            linkText="своих персональных данных"
+            linkHref="/privacy-policy"
+            name="personalData"
+            id="personalData"
+          />
+        </FormRow>
+      </form>
+    </div>
   )
 }
 
