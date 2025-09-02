504d511542060eed5d5ee018d4c310c55089bc84
Justikarx <twin.m999@gmail.com>
2025-09-02 09:54:55 +0700

    Update index.tsx

--- src/components/flatLayoutCard/index.tsx
+++ src/components/flatLayoutCard/index.tsx
@@ -1,74 +1,133 @@
 import clsx from "clsx"
 
+import React from "react"
+
+import Image from "next/image"
+import Link from "next/link"
+
 import styles from "./flatLayoutCard.module.scss"
 
-import Skeleton from "../ui/skeleton"
+import IconImage from "../ui/IconImage"
+import IconButton from "../ui/buttons/IconButton"
 
-interface IFlatLayoutCardSkeletonProps {
+interface IFlatLayoutCardProps {
   listClassName?: string
+  title?: string
+  price?: string
+  complex?: string
+  description?: string[]
+  imageUrl?: string
+  linkUrl?: string
+  apartment?: any
 }
 
-const FlatLayoutCardSkeleton = ({
+const FlatLayoutCard = ({
   listClassName,
-}: IFlatLayoutCardSkeletonProps) => {
+  title = "Студия, 25 м²",
+  price = "4 359 990 ₽",
+  complex = "Европейский берег",
+  description = [
+    "Этаж 8 из 17",
+    "I кв 2025",
+    "Дом кирпичный",
+    "Отделка улучшенная черновая",
+  ],
+  imageUrl = "/images/temporary/room.png",
+  linkUrl = "/details/1",
+  apartment,
+}: IFlatLayoutCardProps) => {
   return (
     <div className={styles.flatLayoutCard}>
       <div className={styles.flatLayoutCard__header}>
-        <Skeleton width="60%" height="16px" />
+        <span>{complex}</span>
         <div className={styles.flatLayoutCard__header__actions}>
-          <Skeleton width="32px" height="32px" border="8px" />
-          <Skeleton width="32px" height="32px" border="8px" />
+          <IconButton
+            size="sm"
+            type="secondary"
+            iconLink="/images/icons/heart.svg"
+          />
+          <IconButton
+            size="sm"
+            type="secondary"
+            iconLink="/images/icons/share.svg"
+          />
         </div>
       </div>
       <div className={styles.flatLayoutCard__content}>
-        <div className={styles.flatLayoutCard__content__image__wrapper}>
+        <Link
+          href={linkUrl}
+          className={styles.flatLayoutCard__content__image__wrapper}
+        >
           <div className={styles.flatLayoutCard__content__image}>
-            <Skeleton width="100%" height="100%" border="12px" />
+            <Image src={imageUrl} alt="flat-layout-card" fill />
           </div>
-        </div>
-        <Skeleton
-          className={styles.flatLayoutCard__content__title}
-          width="80%"
-          height="20px"
-        />
+        </Link>
+        <span className={styles.flatLayoutCard__content__title}>{title}</span>
         <ul
           className={clsx(
             styles.flatLayoutCard__content__description,
             listClassName
           )}
         >
-          {[1, 2, 3, 4].map((index) => (
-            <li
-              key={index}
-              className={styles.flatLayoutCard__content__description__item}
-            >
-              <Skeleton width={`${60 + Math.random() * 40}%`} height="14px" />
-            </li>
+          {description.map((item, index) => (
+            <React.Fragment key={item}>
+              <li className={styles.flatLayoutCard__content__description__item}>
+                {item}
+              </li>
+              {index < description.length - 1 && (
+                <div
+                  className={
+                    styles.flatLayoutCard__content__description__separator
+                  }
+                />
+              )}
+            </React.Fragment>
           ))}
         </ul>
         <div className={styles.flatLayoutCard__content__price}>
-          <Skeleton
-            className={styles.flatLayoutCard__content__price__value}
-            width="70%"
-            height="24px"
-          />
-          <div className={styles.flatLayoutCard__content__price__change}>
+          <h4 className={styles.flatLayoutCard__content__price__value}>
+            {price}
+          </h4>
+          <Link
+            href={linkUrl}
+            className={styles.flatLayoutCard__content__price__change}
+          >
             <div
               className={styles.flatLayoutCard__content__price__change__info}
             >
-              <Skeleton width="16px" height="16px" />
-              <Skeleton width="140px" height="14px" />
+              <IconImage
+                className={
+                  styles.flatLayoutCard__content__price__change__info__icon
+                }
+                iconLink="/images/icons/price-graph.svg"
+                alt="price-graph"
+              />
+              <span
+                className={
+                  styles.flatLayoutCard__content__price__change__info__text
+                }
+              >
+                + 73 350 ₽ изменение цены
+              </span>
             </div>
+
             <div
               className={styles.flatLayoutCard__content__price__change__button}
             >
-              <Skeleton width="20px" height="20px" />
+              <IconImage
+                iconLink="/images/icons/arrow-right.svg"
+                alt="arrow-right"
+                className={
+                  styles.flatLayoutCard__content__price__change__button__icon
+                }
+              />
             </div>
-          </div>
+          </Link>
         </div>
       </div>
     </div>
   )
 }
 
-export default FlatLayoutCardSkeleton
+export default FlatLayoutCard
+
