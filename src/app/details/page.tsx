1d0a06f00df289e3893a8086acdc4ce92bd7882f
Justikarx <twin.m999@gmail.com>
2025-09-02 12:31:08 +0700

    Фикс коммит. Исправлена детальная страница ЖК

--- src/app/details/page.tsx
+++ src/app/details/page.tsx
@@ -2,157 +2,24 @@
 
 import React from "react"
 
-import { useSearchParams } from "next/navigation"
-
 import { MapProvider } from "@/providers/map-provider"
 import { IAboutObjectItem } from "@/types/Object"
-import { ApartmentResponse } from "@/types/api/apartment"
-import { IDoc, IDocInclude, IInclude } from "@/types/api/apartment"
-import { IResidentialComplexInclude } from "@/types/api/apartment"
+import { ResidentialComplexDataResponse } from "@/types/api/complex"
 import { useApiQuery } from "@/utils/hooks/use-api"
 
 import styles from "./details.module.scss"
 
-import Compilation from "../components/compilation"
-import Location from "../details/components/location"
-import AboutComplexSkeleton from "../detailsFlat/components/aboutComplex/AboutComplexSkeleton"
-import DocumentsSkeleton from "../detailsFlat/components/documents/DocumentsSkeleton"
 import AboutComplex from "./components/aboutComplex"
 import AboutObject from "./components/aboutObject"
 import AboutObjectSmall from "./components/aboutObjectSmall"
 import ConstructionProgress from "./components/constructionProgress"
 import Documents from "./components/documents"
 import Estate from "./components/estate"
+import FlatList from "./components/flatList"
 import DetailsHeader from "./components/header"
+import Location from "./components/location"
 
-const FLAT_KEY = "cdcaa3c4-822d-11f0-8411-10f60a82b815"
-
-const DetailsPage = () => {
-  const searchParams = useSearchParams()
-  const keyFromUrl = searchParams.get("key")
-  const flatKey = keyFromUrl || FLAT_KEY
-
-  const {
-    data: apartmentData,
-    isLoading: apartmentLoading,
-    isError: apartmentError,
-  } = useApiQuery<ApartmentResponse>(
-    ["apartments", flatKey],
-    `/apartments/read?key=${flatKey}&includes=ResidentialComplex,Doc,Building`,
-    {
-      staleTime: 5 * 60 * 1000,
-      gcTime: 10 * 60 * 1000,
-    }
-  )
-  const isResidentialComplexInclude = (
-    include: IInclude
-  ): include is IResidentialComplexInclude => {
-    return include.type === "residentialcomplex"
-  }
-  const isDocsInclude = (include: IInclude): include is IDocInclude => {
-    return include.type === "doc"
-  }
-
-  return (
-    <div className={styles.details}>
-      <DetailsHeader
-        data={
-          apartmentData?.attributes
-            ? {
-                name: apartmentData.attributes.h1 ?? "",
-                address:
-                  apartmentData.attributes.includes?.find(
-                    isResidentialComplexInclude
-                  )?.attributes?.[0]?.h1 ?? "",
-                metroStation:
-                  apartmentData.attributes.includes?.find(
-                    isResidentialComplexInclude
-                  )?.attributes?.[0]?.metro_station ?? "",
-                metroType:
-                  apartmentData.attributes.includes?.find(
-                    isResidentialComplexInclude
-                  )?.attributes?.[0]?.metro_type ?? "",
-                metroTime:
-                  apartmentData.attributes.includes?.find(
-                    isResidentialComplexInclude
-                  )?.attributes?.[0]?.metro_time ?? 0,
-              }
-            : null
-        }
-        isLoading={apartmentLoading}
-        isError={apartmentError}
-      />
-      <Estate />
-
-      <AboutObject items={aboutObjectItems} />
-      {apartmentLoading ? (
-        <AboutComplexSkeleton />
-      ) : (
-        <AboutComplex
-          data={
-            apartmentData?.attributes?.includes?.find(
-              isResidentialComplexInclude
-            )
-              ? {
-                  description:
-                    apartmentData.attributes.includes.find(
-                      isResidentialComplexInclude
-                    )?.attributes?.[0]?.description ?? "",
-                }
-              : null
-          }
-          isLoading={apartmentLoading}
-          isError={apartmentError}
-        />
-      )}
-      <MapProvider>
-        <Location
-          latitude={
-            apartmentData?.attributes?.includes?.find(
-              isResidentialComplexInclude
-            )?.attributes?.[0]?.latitude ?? 0
-          }
-          longitude={
-            apartmentData?.attributes?.includes?.find(
-              isResidentialComplexInclude
-            )?.attributes?.[0]?.longitude ?? 0
-          }
-          complexName={
-            apartmentData?.attributes?.includes?.find(
-              isResidentialComplexInclude
-            )?.attributes?.[0]?.h1 ?? ""
-          }
-        />
-      </MapProvider>
-      <ConstructionProgress />
-      {apartmentLoading ? <DocumentsSkeleton /> : <Documents />}
-      <Compilation
-        header="Похожие квартиры"
-        hasPromoCard={false}
-        similarApartmentParams={
-          apartmentData?.attributes
-            ? {
-                city_code: "novosibirsk",
-                city: "novosibirsk",
-                price: apartmentData.attributes.price,
-                area: apartmentData.attributes.area,
-                living_space: apartmentData.attributes.living_space,
-                kitchen_space: apartmentData.attributes.kitchen_space,
-                room_count: apartmentData.attributes.room_count,
-                divergence: 5,
-                exclude_key: apartmentData.attributes.key,
-                exclude_offer_id: apartmentData.attributes.offer_id,
-                page: 1,
-                per_page: 15,
-              }
-            : undefined
-        }
-      />
-    </div>
-  )
-}
-
-export default DetailsPage
+const RESIDENTIAL_COMPLEX_KEY = "e92e332a-822b-11f0-8411-10f60a82b815"
 
 const aboutObjectItems: IAboutObjectItem[] = [
   {
@@ -186,3 +53,114 @@ const aboutObjectItems: IAboutObjectItem[] = [
     icon: "/images/icons/building.svg",
   },
 ]
+
+const aboutObjectItemsSmall: IAboutObjectItem[] = [
+  {
+    title: "Транспортная доступность",
+    icon: "/images/icons/wall.svg",
+  },
+  {
+    title: "Развитая инфраструктура",
+    icon: "/images/icons/height.svg",
+  },
+  {
+    title: "Благоустроенная придомовая территория",
+    icon: "/images/icons/elevator.svg",
+  },
+  {
+    title: "Просторные колясочные",
+    icon: "/images/icons/stairs.svg",
+  },
+  {
+    title: "Детские сады и школы",
+    icon: "/images/icons/parking.svg",
+  },
+]
+
+const DetailsPage = () => {
+  const FULL_API_URL = `http://localhost:1080/api/v1/residential-complex/read?key=${RESIDENTIAL_COMPLEX_KEY}`
+
+  const {
+    data: complexData,
+    isLoading,
+    error,
+  } = useApiQuery<ResidentialComplexDataResponse>(
+    ["residential-complex", RESIDENTIAL_COMPLEX_KEY],
+    FULL_API_URL,
+    {
+      staleTime: 5 * 60 * 1000,
+      gcTime: 10 * 60 * 1000,
+    }
+  )
+  if (isLoading) {
+    return (
+      <div className={styles.details}>
+        <DetailsHeader isLoading={true} />
+        <Estate />
+        <FlatList
+          complexKey={RESIDENTIAL_COMPLEX_KEY}
+        />
+        <AboutObject items={aboutObjectItems} />
+        <AboutComplex isLoading={true} />
+        <MapProvider>
+          <Location />
+        </MapProvider>
+        <AboutObjectSmall items={aboutObjectItemsSmall} />
+        <ConstructionProgress />
+        <Documents />
+      </div>
+    )
+  }
+  if (error || !complexData) {
+    return (
+      <div className={styles.details}>
+        <DetailsHeader isError={true} />
+        <Estate />
+        <FlatList
+          complexKey={RESIDENTIAL_COMPLEX_KEY}
+        />
+        <AboutObject items={aboutObjectItems} />
+        <AboutComplex />
+        <MapProvider>
+          <Location />
+        </MapProvider>
+        <AboutObjectSmall items={aboutObjectItemsSmall} />
+        <ConstructionProgress />
+        <Documents />
+      </div>
+    )
+  }
+  const headerData = {
+    name: complexData.attributes.name,
+    address: complexData.attributes.address,
+    metroStation: complexData.attributes.metro_station,
+    metroType: complexData.attributes.metro_type,
+    metroTime: complexData.attributes.metro_time,
+  }
+  const aboutComplexData = {
+    description: complexData.attributes.description,
+  }
+  return (
+    <div className={styles.details}>
+      <DetailsHeader data={headerData} />
+      <Estate />
+      <FlatList
+        complexKey={RESIDENTIAL_COMPLEX_KEY}
+      />
+      <AboutObject items={aboutObjectItems} />
+      <AboutComplex data={aboutComplexData} />
+      <MapProvider>
+        <Location
+          latitude={complexData.attributes.latitude}
+          longitude={complexData.attributes.longitude}
+          complexName={complexData.attributes.name}
+        />
+      </MapProvider>
+      <AboutObjectSmall items={aboutObjectItemsSmall} />
+      <ConstructionProgress />
+      <Documents />
+    </div>
+  )
+}
+
+export default DetailsPage
