942048e1e942082c7c8001dacee851664ea50309
Justikarx <twin.m999@gmail.com>
2025-09-03 15:00:33 +0700

    Промежуточный коммит: начало работы

--- src/app/details/components/flatList/index.tsx
+++ src/app/details/components/flatList/index.tsx
@@ -16,7 +16,7 @@ interface FlatListProps {
 }
 
 const FlatList: FC<FlatListProps> = ({ complexKey }) => {
-  const FULL_API_URL = `http://localhost:1080/api/v1/residential-complex/read?key=${complexKey}&includes=Apartment`
+  const FULL_API_URL = `http://localhost:1080/api/v1/residential-complex/read?key=${complexKey}&includes=Apartment&filter=apartments.room`
   const {
     data: flatListData,
     isLoading,
@@ -62,7 +62,6 @@ const FlatList: FC<FlatListProps> = ({ complexKey }) => {
 
       <LayoutList
         apartments={
-
           (Array.isArray(flatListData.attributes)
             ? flatListData.attributes[0]
             : flatListData.attributes
